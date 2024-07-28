fc059b429102b573f3cffde710d7865edf2cbc98
ntwaliclinton <ntwalibillclinton@gmail.com>
2024-07-28 02:13:45 -0700

    Add SQL script to insert a single row into the Customers table

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
--- Insert a single row into the Customers table, ignoring duplicates
-INSERT IGNORE INTO Customers (customer_id, customer_name, email, address)
+-- Insert a single row into the Customers table
+INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
 
