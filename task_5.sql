fc795435dbd5e8045665d0e8aebdf36db532638e
ntwaliclinton <ntwalibillclinton@gmail.com>
2024-07-28 02:28:49 -0700

    Add SQL script to insert a single row into the Customers table

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,6 @@
+-- Delete existing row if any
+DELETE FROM Customers WHERE customer_id = 1;
+
 -- Insert a single row into the Customers table
 INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
